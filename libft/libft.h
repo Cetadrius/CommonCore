17f41a2fab4268a274242c3c32bb8d5f15c8ddac
Cetadrius <vcetadrius@gmail.com>
2024-10-24 16:01:10 +0100

    Add files via upload

--- libft/libft.h
+++ libft/libft.h
@@ -18,7 +18,7 @@
 # include <limits.h>
 # include <stdint.h>
 
-void	bzero(void *s, size_t n);
+void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);

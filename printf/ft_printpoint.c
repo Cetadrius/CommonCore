563ed3df7402586f10294ae1aa89a2afe5f91a9f
Cetadrius <vcetadrius@gmail.com>
2024-11-07 13:17:10 +0000

    Add files via upload

--- printf/ft_printpoint.c
+++ printf/ft_printpoint.c
@@ -6,26 +6,41 @@
 /*   By: afilipe- <afilipe-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 09:55:23 by afilipe-          #+#    #+#             */
-/*   Updated: 2024/11/05 13:35:07 by afilipe-         ###   ########.fr       */
+/*   Updated: 2024/11/07 11:43:58 by afilipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	ft_putpoint(unsigned long p)
+{
+	char	*base;
+	int			count;
+
+	count = 0;
+	base = "0123456789abcdef";
+	if (p >= 16)
+	{
+		count += ft_putpoint(p / 16);
+	}
+	count += ft_printc(base[p % 16]);
+	return (count);
+}
 
 int	ft_printptr(unsigned long pointer)
 {
 	int	l;
 
 	l = 0;
-	if (!pointer)
+	if (pointer == 0)
 	{
-		ft_printstr("(nill)");
+		ft_printstr("(nil)");
 		return (5);
 	}
 	else
 	{
 		l += ft_printstr("0x");
-		l += ft_phex(pointer, 'x');
+		l += ft_putpoint(pointer);
 	}
 	return (l);
 }
